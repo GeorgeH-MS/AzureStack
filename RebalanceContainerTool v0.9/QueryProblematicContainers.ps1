b05a31d3bacf1bf46fffff0f13c7e393759704ba
GeorgeH-MS <30331846+GeorgeH-MS@users.noreply.github.com>
2019-02-15 15:16:49 +0800

    Update QueryProblematicContainers.ps1

--- RebalanceContainerTool v0.9/QueryProblematicContainers.ps1
+++ RebalanceContainerTool v0.9/QueryProblematicContainers.ps1
@@ -1,4 +1,4 @@
-﻿$farm = Get-AzsStorageFarm
+$farm = Get-AzsStorageFarm
 $disks = ( Get-AzsStorageAcquisition -FarmName $farm.name | where Storageaccount -notlike 'md-*' | select Susbcriptionid, Storageaccount, Container, Blob, Acquisitionid, FilePath )
 $pcontainers = $disks | group Susbcriptionid, StorageAccount, Container | where Count -gt 1
 
@@ -6,19 +6,12 @@ if( $pcontainers.Count -gt 0 )
 {
     $SubscriptionMap = @{}
     $folderName = "output_" + $farm.Name
-    New-Item ($folderName) -ItemType Directory
+    if (!(Test-Path -Path $folderName))
+    {
+        New-Item ($folderName) -ItemType Directory | Out-Null
+    }
     foreach( $container in $pcontainers )
     {
-        #$SubscriptionId = $container.Name.split(",")[0]
-        #if ($SubscriptionMap.ContainsKey($SubscriptionId))
-        #{
-        #    $SubscriptionMap.$SubscriptionId.Add($container.Name, $container.Group)
-        #}
-        #else
-        #{
-        #    $containerMap = @{$container.Name= $container.Group}
-        #    $SubscriptionMap.Add($SubscriptionId, $containerMap)
-        #}
         foreach( $disk in $container.Group )
         {
             $SubscriptionId = $disk.Susbcriptionid
@@ -33,17 +26,32 @@ if( $pcontainers.Count -gt 0 )
             }
         }
     }
+    $resultfiles = @()
     foreach ($key in $SubscriptionMap.Keys)
     {
         $Subscription = Get-AzsUserSubscription -SubscriptionId $key
-        $OutputFileName = $folderName + "\" + $Subscription.Owner + " " + $key + ".txt"
-        $Disks = @()
-        foreach($key1 in $SubscriptionMap[$key].Keys)
+        if (-not ($Subscription.RoutingResourceManagerType -eq "Admin"))
         {
-            $Disks += $SubscriptionMap[$key][$key1]
+            $OutputFileName = $folderName + "\" + $Subscription.Owner + " " + $key + ".txt"
+            $Disks = @()
+            foreach($key1 in $SubscriptionMap[$key].Keys)
+            {
+                $Disks += $SubscriptionMap[$key][$key1]
+            }
+            $Disks | ConvertTo-Json | Out-File $OutputFileName
+        
+            $result = New-Object PSObject -Property @{
+                SubscriptionOwner = $Subscription.Owner
+                SubscriptionName = $Subscription.DisplayName
+                SubscriptionId = $key
+                VMCount = ($disks | group Acquisitionid).Count
+                DisksCount = $disks.Count
+                DiskConfigFile = $Subscription.Owner + " " + $key + ".txt"
+            }
+            $resultfiles += $result
         }
-        $Disks | ConvertTo-Json | Out-File $OutputFileName
-        Write-Host "Add " $Subscription.DisplayName " to " $OutputFileName
     }
 }
 
+$resultfiles | select SubscriptionOwner, SubscriptionName, SubscriptionId, VMCount, DisksCount, DiskConfigFile | FT | Write-Output
+Write-Host "Above subscriptions have problematic containers. Configuration files for each subscription generated under folder "$folderName". Please contact the subscription owner with the configuration file, and ask owners to run the analyze disk tool for each subscription to resolve the impacted VMs. And then run rebalance disk tool to distribute the container allocation for identified disks."
